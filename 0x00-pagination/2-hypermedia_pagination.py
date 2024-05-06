48af49a9df50eb366badf896cfec70b65a3be5c0
Axcis380 <133226284+Axcis380@users.noreply.github.com>
2024-05-06 02:32:40 +0100

    Update 2-hypermedia_pagination.py

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Task 2: Hypermedia pagination
+"""Task 2 Hyper-media  pagination
 """
 
 import csv
@@ -8,14 +8,14 @@ from typing import Dict, List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Retrieves the index range from a given page and page size.
+    """Retrieve the index range from given page size.
     """
 
     return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
+    """Server class to paginate database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -23,7 +23,7 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
+        """Cache dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -34,7 +34,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Retrieves a page of data.
+        """Retrieve a page of data.
         """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
@@ -45,7 +45,7 @@ class Server:
         return data[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-        """Retrieves information about a page.
+        """Retrieve information abou page.
         """
         data = self.get_page(page, page_size)
         start, end = index_range(page, page_size)
